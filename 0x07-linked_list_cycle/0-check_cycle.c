8e597c34369e63dbce17dcddece464393d77e12b
steven-cruz <stevencruz.300008@gmail.com>
2021-02-04 23:28:34 -0500

    fix

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -1,32 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * check_cycle - function that checks if a singly linked list has a cycle in it.
+ * check_cycle - function that checks if a singly linked
+ * list has a cycle in it.
  *
- * @list: pointer. 
+ * @list: pointer.
  *
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list->next;
 
-	if (list == NULL || !(list->next))
+	listint_t *tortoise, *hare;
+
+	if (!list)
 		return (0);
-	while ((fast->next) != NULL)
+
+	tortoise = list;
+	hare = list;
+
+	while (tortoise && hare && hare->next)
 	{
-		fast = fast->next;
-		if (fast->next)
-			fast = fast->next;
-		else
-			return (0);
-		slow = slow->next;
-		if (slow == fast)
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+
+		if (hare == tortoise)
 			return (1);
 	}
+
 	return (0);
 }
