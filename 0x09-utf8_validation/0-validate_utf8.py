7313f983eb284ad012de0e44ab7e0d0075dd8a14
steven-cruz <stevencruz.300008@gmail.com>
2021-02-21 17:09:34 -0500

    UTF-8 Validation

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -9,19 +9,20 @@
 def validUTF8(data):
         '''Check UTF-8'''
         bytes = 0
-    for num in data:
-        binary = format(num, '#010b')[-8:]
-        if bytes == 0:
-            for bit in binary:
-                if bit == '0':
-                    break
-                bytes += 1
+        for num in data:
+            binary = format(num, '#010b')[-8:]
             if bytes == 0:
-                continue
-            if bytes == 1 or bytes > 4:
-                return False
-        else:
-            if not (binary[0] == '1' and binary[1] == '0'):
-                return False
-        bytes -= 1
-    return bytes == 0
+                for bit in binary:
+                    if bit == '0':
+                        break
+                    bytes += 1
+                if bytes == 0:
+                    continue
+                if bytes == 1 or bytes > 4:
+                    return False
+            else:
+                if not (binary[0] == '1' and binary[1] == '0'):
+                    return False
+            bytes -= 1
+        return bytes == 0
+        
