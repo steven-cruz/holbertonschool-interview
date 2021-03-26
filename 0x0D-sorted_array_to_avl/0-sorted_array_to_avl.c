e72bd5d658d3a4161104b2d1a9a50c74950415e0
steven-cruz <stevencruz.300008@gmail.com>
2021-03-25 22:33:01 -0500

    AVL l From sorted array

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,78 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ *
+ * Return: New Node or NULL if fail
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->n = value;
+
+	return (new_node);
+}
+/**
+ * sort_insert - Auxiliary Function for sort insertion
+ * @array: pointer to the first element of the array to be converted
+ * @min: lower limit
+ * @max: upper limit
+ *
+ * Return: pointer to the root node of the created AVL tree, or NULL on failure
+ */
+avl_t *sort_insert(int *array, int min, int max)
+{
+	int half;
+	avl_t *tree;
+
+	if (min > max)
+		return (NULL);
+
+	half = (max + min) / 2;
+
+	tree = binary_tree_node(NULL, array[half]);
+	if (!tree)
+		return (NULL);
+
+	tree->left = sort_insert(array, min, half - 1);
+
+	tree->right = sort_insert(array, half + 1, max);
+
+	if (tree->left)
+		tree->left->parent = tree;
+
+	if (tree->right)
+		tree->right->parent = tree;
+
+	return (tree);
+}
+
+/**
+ * sorted_array_to_avl - builds an AVL tree from an array
+ * @array: pointer to the first element of the array to be converted
+ * @size: number of element in the array
+ *
+ * Return: pointer to the root node of the created AVL tree, or NULL on failure
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	avl_t *root = NULL;
+
+	if (!array || size < 1)
+		return (NULL);
+
+	root = sort_insert(array, 0, size - 1);
+
+	return (root);
+}
