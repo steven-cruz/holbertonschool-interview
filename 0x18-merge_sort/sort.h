89a56a1df2d19e3f7b0f3ef409ae331cc113b5ce
steven-cruz <stevencruz.300008@gmail.com>
2021-06-10 22:09:38 -0500

    marge sort

---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,15 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+/* Print Function */
+
+void print_array(const int *array, size_t size);
+
+/* Sort Function */
+
+void merge_sort(int *array, size_t size);
+
+#endif /* SORT_H */
